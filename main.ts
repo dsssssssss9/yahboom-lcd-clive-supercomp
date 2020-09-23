afc42ce533fdd1455b87e0bead47742a895c1993
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-23 23:21:14 +0100

    Restore '23/09/2020, 23:17:47 Restore '23/09/2020, 22:53:56 Initial files for MakeCode project''

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 LEDBit.LEDClear()
 LEDBit.LEDAllOn()
 basic.forever(function () {
-	
-})
-control.inBackground(function () {
-    while (true) {
-        LEDBit.LEDDraw(randint(0, 15), randint(0, 7), LEDBit.enState.ON)
-        LEDBit.LEDDraw(randint(0, 15), randint(0, 7), LEDBit.enState.OFF)
-    }
+    LEDBit.LEDDraw(randint(0, 15), randint(0, 7), LEDBit.enState.ON)
+    LEDBit.LEDDraw(randint(0, 15), randint(0, 7), LEDBit.enState.OFF)
 })
