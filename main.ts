b88d28b7013f7e9b89aabc5261e69140ebc39694
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-03-23 20:57:20 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 LEDBit.LEDClear()
 LEDBit.LEDAllOn()
 basic.forever(function () {
-    basic.showString("B.C.S.C     B.C.S.C     This is a mini  version of the Big Clive SuperComputer, ")
-    basic.showString("Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!!   Hello Family!   Hello Family!   Hello Family!   Hello Family")
-    basic.showString("Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!")
+    while (true) {
+        basic.showString("Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!!   Hello Family!   Hello Family!   Hello Family!   Hello Family")
+        basic.showString("B.C.S.C     B.C.S.C     This is a mini  version of the Big Clive SuperComputer, ")
+        basic.showString("rink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!")
+    }
 })
 control.inBackground(function () {
     while (true) {
