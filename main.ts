1d7a46b9282b20c5cc003bb52fca0f622fc1205e
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-03-24 00:17:18 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ basic.forever(function () {
     while (true) {
         basic.showString("Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!! Hello Big Clive!!!   Hello Family!   Hello Family!   Hello Family!   Hello Family")
         basic.showString("B.C.S.C     B.C.S.C     This is a mini  version of the Big Clive SuperComputer, ")
-        basic.showString("rink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!")
+        basic.showString("Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!  Drink Radium Gin!")
     }
 })
 control.inBackground(function () {
